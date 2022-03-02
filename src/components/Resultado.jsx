4ae90fa38c002a90fa5c500c61a0e1bf69399c19
dar96 <darielfriascastillo@gmail.com>
2022-03-02 09:31:53 +0100

    Precio más bajo

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -38,7 +38,7 @@ const Resultado = ({resultado}) => {
             <div>
                 <Precio>El precio es de: <span>{PRICE}</span></Precio>
                 <Texto>Precio más alto del día: <span>{HIGHDAY}</span></Texto>
-                <Texto>Precio más alto del día: <span>{LOWDAY}</span></Texto>
+                <Texto>Precio más bajo del día: <span>{LOWDAY}</span></Texto>
                 <Texto>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Texto>
                 <Texto>Última actualización: <span>{LASTUPDATE}</span></Texto>
             </div>
